214eb2463d0b2a722c358c55cb604b9ded3c319f
Michael Gorbunov <mg0rby@yandex.ru>
2024-11-22 11:49:31 +0300

    сериалайзер количество уроков в курсе

--- lms/serializer.py
+++ lms/serializer.py
@@ -4,6 +4,12 @@ from lms.models import Course, Lesson
 
 
 class CourseSerializer(serializers.ModelSerializer):
+
+    lesson_count = serializers.SerializerMethodField(read_only=True)
+
+    def get_lesson_count(self, instance):
+        return instance.lesson_set.all().count()
+
     class Meta:
         model = Course
         fields = "__all__"
