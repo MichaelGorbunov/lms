ae7a99e0a4f47ccf7beadb4fe670e74afa58308f
Michael Gorbunov <mg0rby@yandex.ru>
2024-11-22 21:23:43 +0300

    вывод в профиле пользователя истории платежей вар1

--- users/serializer.py
+++ users/serializer.py
@@ -1,9 +1,16 @@
 from rest_framework import serializers
 
-from users.models import CustomUser,Payments
+from users.models import CustomUser, Payments
 
 
 class CustomUserSerializer(serializers.ModelSerializer):
+    payment_list = serializers.SerializerMethodField(read_only=True)
+
+    def get_payment_list(self, customuser):
+        return [payment.date_pay for payment in Payments.objects.filter(user_pay=customuser)]
+
+    # payment.date_pay,
+
     class Meta:
         model = CustomUser
         fields = "__all__"
@@ -13,5 +20,3 @@ class PaymentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Payments
         fields = "__all__"
-
-
