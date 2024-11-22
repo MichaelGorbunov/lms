880fb6200e086dd908f8ba0864ea6b57b89ea619
Michael Gorbunov <mg0rby@yandex.ru>
2024-11-22 19:24:00 +0300

    вывод курса вариант

--- lms/views.py
+++ lms/views.py
@@ -17,8 +17,9 @@ class LessonCreateAPIView(generics.CreateAPIView):
 
 class LessonListAPIView(generics.ListAPIView):
     serializer_class = LessonSerializer
-    queryset = Lesson.objects.all()
-
+    # queryset = Lesson.objects.all()
+    # books_with_authors = Book.objects.select_related('author').all()
+    queryset = Lesson.objects.select_related("course").all()
 
 class LessonRetrieveAPIView(generics.RetrieveAPIView):
     serializer_class = LessonSerializer
