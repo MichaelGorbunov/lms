17cbb05dd2100199b40d661137b085bf85f1258a
Michael Gorbunov <mg0rby@yandex.ru>
2024-11-23 16:29:51 +0300

    вывод всей информации по урокам

--- lms/serializer.py
+++ lms/serializer.py
@@ -1,24 +1,24 @@
 from rest_framework import serializers
-
 from lms.models import Course, Lesson
 
+
+class LessonSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Lesson
+        fields = "__all__"
 
 
 class CourseSerializer(serializers.ModelSerializer):
     lesson_count = serializers.SerializerMethodField(read_only=True)
-    lesson_list=serializers.SerializerMethodField(read_only=True)
-    def get_lesson_count(self, instance):
-        return instance.lesson_set.all().count()
-    def get_lesson_list(self,course):
-        return [lesson.title for lesson in Lesson.objects.filter(course=course)]
+    lesson_list = serializers.SerializerMethodField()
+
+    def get_lesson_count(self, obj):
+        return obj.lessons.count()
+
+    def get_lesson_list(self, obj):
+        # Получение информации об уроках, связанных с данным курсом
+        return LessonSerializer(obj.lessons.all(), many=True).data
 
     class Meta:
         model = Course
-        fields = "__all__"
-
-
-class LessonSerializer(serializers.ModelSerializer):
-
-    class Meta:
-        model = Lesson
         fields = "__all__"
--- users/serializer.py
+++ users/serializer.py
@@ -3,20 +3,18 @@ from rest_framework import serializers
 from users.models import CustomUser, Payments
 
 
-class CustomUserSerializer(serializers.ModelSerializer):
-    payment_list = serializers.SerializerMethodField(read_only=True)
-
-    def get_payment_list(self, customuser):
-        return [payment.date_pay for payment in Payments.objects.filter(user_pay=customuser)]
-
-    # payment.date_pay,
-
-    class Meta:
-        model = CustomUser
-        fields = "__all__"
-
-
 class PaymentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Payments
+        fields = "__all__"
+
+
+class CustomUserSerializer(serializers.ModelSerializer):
+    payment_list = serializers.SerializerMethodField(read_only=True)
+    def get_payment_list(self, customuser):
+        return [payment.date_pay for payment in Payments.objects.filter(user_pay=customuser)]
+
+
+    class Meta:
+        model = CustomUser
         fields = "__all__"
