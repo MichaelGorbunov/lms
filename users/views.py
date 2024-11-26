4224633d1d8282906a96b4a4fe907c4486809174
Michael Gorbunov <mg0rby@yandex.ru>
2024-11-26 18:56:22 +0300

    create allowany

--- users/views.py
+++ users/views.py
@@ -4,6 +4,10 @@ from django_filters.rest_framework import DjangoFilterBackend
 from users.models import CustomUser, Payments
 from users.serializer import CustomUserSerializer, PaymentSerializer
 from rest_framework.filters import OrderingFilter
+from rest_framework import viewsets, permissions, status
+from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
+from django.contrib.auth.models import User
 
 
 class CustomUserViewSet(viewsets.ModelViewSet):
@@ -11,6 +15,23 @@ class CustomUserViewSet(viewsets.ModelViewSet):
 
     serializer_class = CustomUserSerializer
     queryset = CustomUser.objects.all()
+    # permission_classes = [AllowAny]  # Позволяем создавать пользователей без авторизации
+
+    def perform_create(self, serializer):
+        # Создаем пользователя с указанными данными и устанавливаем активность
+        user = serializer.save(is_active=True)
+        # Устанавливаем хешированный пароль
+        user.set_password(user.password)
+        user.save()
+
+    def get_permissions(self):
+        # Получаем список разрешений, в зависимости от типа запроса
+        if self.action in ['create']:  # Если действие - создание пользователя
+            permission_classes = [permissions.AllowAny]  # Позволяем всем доступ к этому действию
+        else:  # Для остальных действий (retrieve, update, delete и т.д.)
+            permission_classes = [permissions.IsAuthenticated]  # Требуем аутентификацию
+
+        return [permission() for permission in permission_classes]
 
 
 class PaymentsViewSet(viewsets.ModelViewSet):
