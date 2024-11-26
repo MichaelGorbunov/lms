420108b772658616cd5e734405b1e9f79192a47c
Michael Gorbunov <mg0rby@yandex.ru>
2024-11-26 17:21:20 +0300

    init hw_31,add djangorestframework-simplejwt

--- drf.cmd
+++ drf.cmd
@@ -5,4 +5,8 @@ poetry add --group dev pillow
 poetry add --group dev django
 poetry add --group dev python-dotenv
 poetry add --group dev djangorestframework
+poetry add --group dev django-filter
+poetry add --group dev djangorestframework-simplejwt
+
+
 
