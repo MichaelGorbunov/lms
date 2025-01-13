80028561a602aff97c6037863720f5c4fd0b2373
Michael Gorbunov <mg0rby@yandex.ru>
2025-01-13 19:03:00 +0300

    deploy static

--- config/settings.py
+++ config/settings.py
@@ -129,12 +129,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = "static/"
-STATICFILES_DIRS = [
-    # Здесь вы можете добавить пути к вашим статическим файлам,
-    # если они располагаются вне приложений
-    BASE_DIR
-    / "static"
-]
+# STATICFILES_DIRS = [
+#     # Здесь вы можете добавить пути к вашим статическим файлам,
+#     # если они располагаются вне приложений
+#     BASE_DIR
+#     / "static"
+# ]
+STATIC_ROOT = BASE_DIR / 'static'
+
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 MEDIA_URL = "/media/"
